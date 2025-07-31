2b342c9ee162fdbb7ae259405d7ed9bccba1f23f
saialu8391 <saialu8391@gmail.comm>
2025-07-30 22:05:05 -0500

    Update README.md

--- main.py
+++ main.py
@@ -3,3 +3,52 @@ print("Hello World!")
 a = 5
 b = 6
 print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+
+# Print an integer
+print(5)
+
+# Print a float
+print(3.14)
+
+# Print result of addition
+print(2 + 3)
+
+# Print result of subtraction
+print(10 - 4)
+
+# Print result of multiplication
+print(7 * 8)
+
+# Print result of division
+print(15 / 4)
+
+# Print result of floor division
+print(15 // 4)
+
+# Print remainder using modulo
+print(15 % 4)
+
+# Print result of exponentiation
+print(2 ** 4)
+
+# Print result of a square root
+print(16 ** 0.5)
+
+# Print variable value
+a = 100
+print(a)
+
+# Print result of variable operation
+tax_rate = 0.1
+my_taxes = a * tax_rate
+print(my_taxes)
+
+# Print expression with order of operations
+print(2 + 3 * 4)
+
+# Print expression using parentheses
+print((2 + 3) * 4)
