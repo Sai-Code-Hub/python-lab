6466b9a51bbb0cd573e3d150007e05b90b0ac88f
saialu8391 <saialu8391@gmail.comm>
2025-08-04 18:15:55 -0500

    Update README.md

--- 04-Lists.py
+++ 04-Lists.py
@@ -5,8 +5,7 @@
 # 3.) Basic List Methods
 # 4.) Nesting Lists
 # 5.) Introduction to List Comprehensions
-
-
+import time
 
 # 1. Creating Lists
 my_list = [1, 2, 3]
@@ -32,7 +31,14 @@ another_list = ['four', 'five', 'six']
 print(my_list1 + another_list)
 new_list=my_list1+another_list
 print("new_list:", new_list)
-
+new_list[0]='ONE ALL CAP'
+print("new_list:", new_list)
+new_list.append('append-seven')
+print("new_list:", new_list)
+removed_item=new_list.pop()
+timestamp=time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
+print(f"'pop item:::{removed_item}' was removed at {timestamp}")
+#print("removed_items:::", removed_item)
 
 # Concatenation
 print("Concatenated list:", my_list + ['new item'])
@@ -64,12 +70,29 @@ print("Remaining list:", list1)
 # Sorting and Reversing
 new_list = ['a', 'e', 'x', 'b', 'c']
 print("Original new_list:", new_list)
+new_list.sort()
+print("Sorted list:", new_list)
+my_sorted_list = new_list.sort() #sorting
+print("Original new_list:", my_sorted_list)
+
+new_list.sort()
+my_sorted_list = new_list
+print("Sorted new_list:", my_sorted_list)
 
 new_list.reverse()
 print("Reversed list:", new_list)
 
-new_list.sort()
-print("Sorted list:", new_list)
+#numlist-->>
+num_list = [4,2,3,1,6,5]
+num_list.sort()
+print("Original num_list:", num_list)
+
+
+num_list.reverse()
+print("Reversed list:", num_list)
+
+# new_list.sort()
+# print("Sorted list:", new_list)
 
 # 4. Nesting Lists
 lst_1 = [1, 2, 3]
@@ -80,8 +103,11 @@ matrix = [lst_1, lst_2, lst_3]
 print("Matrix:", matrix)
 
 print("First row of matrix:", matrix[0])
+print("second row of matrix:", matrix[1])
 print("First element of first row:", matrix[0][0])
 
 # 5. List Comprehensions
 first_col = [row[0] for row in matrix]
 print("First column via list comprehension:", first_col)
+
+#append, pop, sort, reverse
